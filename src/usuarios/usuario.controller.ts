5da147e2f2ca845972b60ef52af84af7d69777a5
Danilo Loose <danloose14@gmail.com>
2023-04-06 16:02:59 -0300

    criar e configurar rotas atualizar e remover user

--- src/usuarios/usuario.controller.ts
+++ src/usuarios/usuario.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { CriaUsuarioDTO } from "./dto/CriaUsuario.dto";
 import { UsuarioEntity } from "./usuario.entity";
 import { v4 as uuid } from 'uuid';
 import { ListaUsuarioDTO } from "./dto/ListaUsuario.DTO";
+import { AtualizaUsuarioDTO } from "./dto/AtualizaUsuarioDTO";
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -27,6 +28,7 @@ export class UsuarioController {
         };
     }
 
+
     @Get()
     async listaUsuarios() {
 
@@ -37,6 +39,26 @@ export class UsuarioController {
 
         return usuariosLista;
 
+    }
+
+    @Put("/:id")
+    async atualizaUsuario(@Param("id") id: string, @Body() novosDados: AtualizaUsuarioDTO) {
+        const usuarioAtualizado = await this.usuarioRepository.atualiza(id, novosDados);
+
+        return {
+            usuario: usuarioAtualizado,
+            message: "Usuário atualizado com sucesso!"
+        }
+    }
+
+    @Delete("/:id")
+    async removeUsuario(@Param("id") id: string) {
+        const usuarioRemovido = await this.usuarioRepository.remove(id);
+
+        return {
+            usuario: usuarioRemovido,
+            message: "O usuário foi removido com sucesso!"
+        }
     }
 
 }
--- src/usuarios/usuario.repository.ts
+++ src/usuarios/usuario.repository.ts
@@ -13,6 +13,41 @@ export class UsuarioRepository {
         return this.usuarios;
     }
 
+    private buscaPorId(id: string) {
+        const possivelUsuario = this.usuarios.find(
+            usuario => usuario.id === id
+        )
+
+        if (!possivelUsuario) {
+            throw new Error("Usuário não existe.");
+        }
+
+        return possivelUsuario;
+    }
+
+    async atualiza(id: string, dadosNovos: Partial<UsuarioEntity>) {
+        const usuario = this.buscaPorId(id);
+
+        Object.entries(dadosNovos).forEach(([chave, valor]) => {
+            if (chave !== "id") {
+                usuario[chave] = valor;
+            }
+        })
+
+        return usuario;
+
+    }
+
+    async remove(id: string) {
+        const usuario = this.buscaPorId(id);
+
+        this.usuarios = this.usuarios.filter(usuario => {
+            usuario => usuario.id !== id
+        })
+
+        return usuario;
+    }
+
     async existeEmail(email: string) {
         const possivelUsuario = this.usuarios.find(
             usuario => usuario.email === email
